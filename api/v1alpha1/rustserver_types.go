46fde08bf63c0440108590c093beb9beb4f0e2d9
Aaron Hulse <theding0x@spaceship-moon.com>
2024-06-10 07:24:49 -0500

    Redo with kubebuilder scaffolding

---
+++ api/v1alpha1/rustserver_types.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2024 theding0x.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// RustServerSpec defines the desired state of RustServer
+type RustServerSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of RustServer. Edit rustserver_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// RustServerStatus defines the observed state of RustServer
+type RustServerStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// RustServer is the Schema for the rustservers API
+type RustServer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RustServerSpec   `json:"spec,omitempty"`
+	Status RustServerStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// RustServerList contains a list of RustServer
+type RustServerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []RustServer `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&RustServer{}, &RustServerList{})
+}
